80eda1b3183e4d85a9a781a9f1405c2c2a8a8db3
Lili <liligg161@gmail.com>
2023-09-05 16:43:39 -0600

    Formulario nuevo

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contacto</title>
+</head>
+<body>
+   <h1> FORMULARIO DE CONTACTOS </h1>
+   <form action =" " method="POST"> 
+    <label for="correo"> Correo </label><br> 
+    
+
+</body>
+</html>
